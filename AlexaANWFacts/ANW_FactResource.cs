e6a6de2b1743d83637cd2db911b731a969ab85dd
Mark-Farrell <Mark.Farrell@coxautoinc.com>
2017-10-04 02:12:27 -0600

    Adding files for alexa skills

---
+++ AlexaANWFacts/ANW_FactResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexaANWFacts
+{
+    public class ANW_FactResource
+    {
+        public ANW_FactResource(string language)
+        {
+            this.Language = language;
+            this.Facts = new List<string>();
+        }
+        public string Language { get; set; }
+        public string SkillName { get; set; }
+        public List<string> Facts { get; set; }
+        public string GetFactMessage { get; set; }
+        public string HelpMessage { get; set; }
+        public string HelpReprompt { get; set; }
+        public string StopMessage { get; set; }
+    }
+
+
+    
+}
---
+++ AlexaANWFacts/GetResource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexaANWFacts
+{
+    public class GetResource
+    {
+        public List<ANW_FactResource> GetResources()
+        {
+            List<ANW_FactResource> resources = new List<ANW_FactResource>();
+            ANW_FactResource enUSResource = new ANW_FactResource("en-US");
+            enUSResource.SkillName = "American Ninja Warrior Season 10 Facts";
+            enUSResource.GetFactMessage = "Here's your face : ";
+            enUSResource.HelpMessage = "You can tell me a American Ninja Warrior Season 10 Fact, or , you can say exit...What can I help you with?";
+            enUSResource.HelpReprompt = "What can I help you with?";
+            enUSResource.StopMessage = "Goodbye!";
+            enUSResource.Facts.Add("");
+            enUSResource.Facts.Add("");
+            enUSResource.Facts.Add("");
+            enUSResource.Facts.Add("");
+            enUSResource.Facts.Add("");
+            enUSResource.Facts.Add("");
+            resources.Add(enUSResource);
+            enUSResource.Facts.Add("");
+
+            return resources;
+        }
+    }
+}
